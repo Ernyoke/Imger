9c926b7396f4e8d38aa2fd8d86f482d351793c5a
Szilagyi Ervin <ervin509@yahoo.com>
2018-03-27 01:10:54 +0300

    Image rotation added.

--- transform/transform.go
+++ transform/transform.go
@@ -1,1 +1,68 @@
 package transform
+
+import (
+	"errors"
+	"github.com/Ernyoke/Imger/utils"
+	"image"
+	"math"
+)
+
+func RotateGray(img *image.Gray, angle int, anchor image.Point, resizeToFit bool) (*image.Gray, error) {
+	size := img.Bounds().Size()
+	if anchor.X < 0 || anchor.Y < 0 || anchor.X > size.X || anchor.Y > size.Y {
+		return nil, errors.New("invalid anchor position")
+	}
+	radians := float64(angle) * (math.Pi / 180)
+	newSize := size
+	if resizeToFit {
+		a := math.Abs(float64(size.X) * math.Sin(radians))
+		b := math.Abs(float64(size.X) * math.Cos(radians))
+		c := math.Abs(float64(size.Y) * math.Sin(radians))
+		d := math.Abs(float64(size.Y) * math.Cos(radians))
+		newSize.X = int(c + b)
+		newSize.Y = int(a + d)
+	}
+	offsetX := (newSize.X - size.X) / 2
+	offsetY := (newSize.Y - size.Y) / 2
+	result := image.NewGray(image.Rect(0, 0, newSize.X, newSize.Y))
+	utils.ParallelForEachPixel(newSize, func(x, y int) {
+		dx := x - anchor.X - offsetX
+		dy := y - anchor.Y - offsetY
+
+		newX := int(math.Floor(math.Cos(radians)*float64(dx) - math.Sin(radians)*float64(dy) + float64(anchor.X)))
+		newY := int(math.Floor(math.Sin(radians)*float64(dx) + math.Cos(radians)*float64(dy) + float64(anchor.Y)))
+
+		result.SetGray(x, y, img.GrayAt(newX, newY))
+	})
+	return result, nil
+}
+
+func RotateRGBA(img *image.RGBA, angle int, anchor image.Point, resizeToFit bool) (*image.RGBA, error) {
+	size := img.Bounds().Size()
+	if anchor.X < 0 || anchor.Y < 0 || anchor.X > size.X || anchor.Y > size.Y {
+		return nil, errors.New("invalid anchor position")
+	}
+	radians := float64(angle) * (math.Pi / 180)
+	newSize := size
+	if resizeToFit {
+		a := math.Abs(float64(size.X) * math.Sin(radians))
+		b := math.Abs(float64(size.X) * math.Cos(radians))
+		c := math.Abs(float64(size.Y) * math.Sin(radians))
+		d := math.Abs(float64(size.Y) * math.Cos(radians))
+		newSize.X = int(c + b)
+		newSize.Y = int(a + d)
+	}
+	offsetX := (newSize.X - size.X) / 2
+	offsetY := (newSize.Y - size.Y) / 2
+	result := image.NewRGBA(image.Rect(0, 0, newSize.X, newSize.Y))
+	utils.ParallelForEachPixel(newSize, func(x, y int) {
+		dx := x - anchor.X - offsetX
+		dy := y - anchor.Y - offsetY
+
+		newX := int(math.Floor(math.Cos(radians)*float64(dx) - math.Sin(radians)*float64(dy) + float64(anchor.X)))
+		newY := int(math.Floor(math.Sin(radians)*float64(dx) + math.Cos(radians)*float64(dy) + float64(anchor.Y)))
+
+		result.SetRGBA(x, y, img.RGBAAt(newX, newY))
+	})
+	return result, nil
+}
--- transform/transform_test.go
+++ transform/transform_test.go
@@ -1,1 +1,88 @@
 package transform
+
+import (
+	"testing"
+	"image"
+	"github.com/Ernyoke/Imger/imgio"
+)
+
+// -----------------------------Acceptance tests------------------------------------
+func setupTestCaseGray(t *testing.T) *image.Gray {
+	path := "../res/girl.jpg"
+	img, err := imgio.ImreadGray(path)
+	if err != nil {
+		t.Errorf("Could not read image from path: %s", path)
+	}
+	return img
+}
+
+func setupTestCaseRGBA(t *testing.T) *image.RGBA {
+	path := "../res/girl.jpg"
+	img, err := imgio.ImreadRGBA(path)
+	if err != nil {
+		t.Errorf("Could not read image from path: %s", path)
+	}
+	return img
+}
+
+func tearDownTestCase(t *testing.T, img image.Image, path string) {
+	err := imgio.Imwrite(img, path)
+	if err != nil {
+		t.Errorf("Could not write image to path: %s", path)
+	}
+}
+
+func Test_Acceptance_RotateGray90(t *testing.T) {
+	gray := setupTestCaseGray(t)
+	actual, err:= RotateGray(gray, 90, image.Point{X: 201, Y: 201}, true)
+	if err != nil {
+		t.Errorf("Should not throw error!")
+	}
+	tearDownTestCase(t, actual, "../res/transform/roateGray90.jpg")
+}
+
+func Test_Acceptance_RotateGray45(t *testing.T) {
+	gray := setupTestCaseGray(t)
+	actual, err:= RotateGray(gray, 45, image.Point{X: 201, Y: 201}, true)
+	if err != nil {
+		t.Errorf("Should not throw error!")
+	}
+	tearDownTestCase(t, actual, "../res/transform/roateGray45.jpg")
+}
+
+func Test_Acceptance_RotateGray22(t *testing.T) {
+	gray := setupTestCaseGray(t)
+	actual, err:= RotateGray(gray, 22, image.Point{X: 201, Y: 201}, true)
+	if err != nil {
+		t.Errorf("Should not throw error!")
+	}
+	tearDownTestCase(t, actual, "../res/transform/roateGray22.jpg")
+}
+
+func Test_Acceptance_RotateRGBA90(t *testing.T) {
+	rgba := setupTestCaseRGBA(t)
+	actual, err:= RotateRGBA(rgba, 90, image.Point{X: 201, Y: 201}, true)
+	if err != nil {
+		t.Errorf("Should not throw error!")
+	}
+	tearDownTestCase(t, actual, "../res/transform/roateRGBA90.jpg")
+}
+
+func Test_Acceptance_RotateRGBA45(t *testing.T) {
+	rgba := setupTestCaseRGBA(t)
+	actual, err:= RotateRGBA(rgba, 45, image.Point{X: 201, Y: 201}, true)
+	if err != nil {
+		t.Errorf("Should not throw error!")
+	}
+	tearDownTestCase(t, actual, "../res/transform/roateRGBA45.jpg")
+}
+
+func Test_Acceptance_RotateRGBA22(t *testing.T) {
+	rgba := setupTestCaseRGBA(t)
+	actual, err:= RotateRGBA(rgba, 22, image.Point{X: 201, Y: 201}, true)
+	if err != nil {
+		t.Errorf("Should not throw error!")
+	}
+	tearDownTestCase(t, actual, "../res/transform/roateRGBA22.jpg")
+}
+
