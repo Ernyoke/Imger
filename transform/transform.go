4e008793370b3e3dc8b208cd44f314e3543f5dc9
Szilagyi Ervin <ervin509@yahoo.com>
2018-03-31 00:26:02 +0300

    Refactoring transorm.go. Added some tests for transform.

--- transform/transform.go
+++ transform/transform.go
@@ -7,62 +7,60 @@ import (
 	"math"
 )
 
-func RotateGray(img *image.Gray, angle int, anchor image.Point, resizeToFit bool) (*image.Gray, error) {
+func RotateGray(img *image.Gray, angle float64, anchor image.Point, resizeToFit bool) (*image.Gray, error) {
 	size := img.Bounds().Size()
 	if anchor.X < 0 || anchor.Y < 0 || anchor.X > size.X || anchor.Y > size.Y {
 		return nil, errors.New("invalid anchor position")
 	}
-	radians := float64(angle) * (math.Pi / 180)
+	radians := angleToRadians(angle)
 	newSize := size
 	if resizeToFit {
-		a := math.Abs(float64(size.X) * math.Sin(radians))
-		b := math.Abs(float64(size.X) * math.Cos(radians))
-		c := math.Abs(float64(size.Y) * math.Sin(radians))
-		d := math.Abs(float64(size.Y) * math.Cos(radians))
-		newSize.X = int(c + b)
-		newSize.Y = int(a + d)
+		newSize = computeFitSize(size, radians)
 	}
-	offsetX := (newSize.X - size.X) / 2
-	offsetY := (newSize.Y - size.Y) / 2
 	result := image.NewGray(image.Rect(0, 0, newSize.X, newSize.Y))
 	utils.ParallelForEachPixel(newSize, func(x, y int) {
-		dx := x - anchor.X - offsetX
-		dy := y - anchor.Y - offsetY
-
-		newX := int(math.Floor(math.Cos(radians)*float64(dx) - math.Sin(radians)*float64(dy) + float64(anchor.X)))
-		newY := int(math.Floor(math.Sin(radians)*float64(dx) + math.Cos(radians)*float64(dy) + float64(anchor.Y)))
-
-		result.SetGray(x, y, img.GrayAt(newX, newY))
+		result.SetGray(x, y, img.GrayAt(getOriginalPixelPosition(x, y, radians, anchor, computeOffset(size, newSize))))
 	})
 	return result, nil
 }
 
-func RotateRGBA(img *image.RGBA, angle int, anchor image.Point, resizeToFit bool) (*image.RGBA, error) {
+func RotateRGBA(img *image.RGBA, angle float64, anchor image.Point, resizeToFit bool) (*image.RGBA, error) {
 	size := img.Bounds().Size()
 	if anchor.X < 0 || anchor.Y < 0 || anchor.X > size.X || anchor.Y > size.Y {
 		return nil, errors.New("invalid anchor position")
 	}
-	radians := float64(angle) * (math.Pi / 180)
+	radians := angleToRadians(angle)
 	newSize := size
 	if resizeToFit {
-		a := math.Abs(float64(size.X) * math.Sin(radians))
-		b := math.Abs(float64(size.X) * math.Cos(radians))
-		c := math.Abs(float64(size.Y) * math.Sin(radians))
-		d := math.Abs(float64(size.Y) * math.Cos(radians))
-		newSize.X = int(c + b)
-		newSize.Y = int(a + d)
+		newSize = computeFitSize(size, radians)
 	}
-	offsetX := (newSize.X - size.X) / 2
-	offsetY := (newSize.Y - size.Y) / 2
 	result := image.NewRGBA(image.Rect(0, 0, newSize.X, newSize.Y))
 	utils.ParallelForEachPixel(newSize, func(x, y int) {
-		dx := x - anchor.X - offsetX
-		dy := y - anchor.Y - offsetY
-
-		newX := int(math.Floor(math.Cos(radians)*float64(dx) - math.Sin(radians)*float64(dy) + float64(anchor.X)))
-		newY := int(math.Floor(math.Sin(radians)*float64(dx) + math.Cos(radians)*float64(dy) + float64(anchor.Y)))
-
-		result.SetRGBA(x, y, img.RGBAAt(newX, newY))
+		result.SetRGBA(x, y, img.RGBAAt(getOriginalPixelPosition(x, y, radians, anchor, computeOffset(size, newSize))))
 	})
 	return result, nil
+}
+
+func angleToRadians(angle float64) float64 {
+	return angle * (math.Pi / 180)
+}
+
+func computeFitSize(size image.Point, radians float64) image.Point {
+	a := math.Abs(float64(size.X) * math.Sin(radians))
+	b := math.Abs(float64(size.X) * math.Cos(radians))
+	c := math.Abs(float64(size.Y) * math.Sin(radians))
+	d := math.Abs(float64(size.Y) * math.Cos(radians))
+	return image.Point{X: int(c + b), Y: int(a + d)}
+}
+
+func computeOffset(size image.Point, fittingSize image.Point) image.Point {
+	return image.Point{X: (fittingSize.X - size.X) / 2, Y: (fittingSize.Y - size.Y) / 2}
+}
+
+func getOriginalPixelPosition(x int, y int, radians float64, anchor image.Point, offset image.Point) (int, int) {
+	dx := x - anchor.X - offset.X
+	dy := y - anchor.Y - offset.Y
+	originalX := int(math.Floor(math.Cos(radians)*float64(dx) - math.Sin(radians)*float64(dy) + float64(anchor.X)))
+	originalY := int(math.Floor(math.Sin(radians)*float64(dx) + math.Cos(radians)*float64(dy) + float64(anchor.Y)))
+	return originalX, originalY
 }
--- transform/transform_test.go
+++ transform/transform_test.go
@@ -1,14 +1,174 @@
 package transform
 
 import (
-	"testing"
-	"image"
 	"github.com/Ernyoke/Imger/imgio"
+	"github.com/Ernyoke/Imger/utils"
+	"image"
+	"math"
+	"testing"
 )
+
+// ---------------------------------Unit tests--------------------------------------
+func Test_AngleToRadians0(t *testing.T) {
+	expected := 0.0
+	actual := angleToRadians(0)
+	if !utils.IsEqualFloat64(actual, expected) {
+		t.Errorf("Expected value %f is not eqaul with actual value %f", expected, actual)
+	}
+}
+
+func Test_AngleToRadians30(t *testing.T) {
+	expected := math.Pi / 6
+	actual := angleToRadians(30)
+	if !utils.IsEqualFloat64(actual, expected) {
+		t.Errorf("Expected value %f is not eqaul with actual value %f", expected, actual)
+	}
+}
+
+func Test_AngleToRadians45(t *testing.T) {
+	expected := math.Pi / 4
+	actual := angleToRadians(45)
+	if !utils.IsEqualFloat64(actual, expected) {
+		t.Errorf("Expected value %f is not eqaul with actual value %f", expected, actual)
+	}
+}
+
+func Test_AngleToRadians60(t *testing.T) {
+	expected := math.Pi / 3
+	actual := angleToRadians(60)
+	if !utils.IsEqualFloat64(actual, expected) {
+		t.Errorf("Expected value %f is not eqaul with actual value %f", expected, actual)
+	}
+}
+
+func Test_AngleToRadians90(t *testing.T) {
+	expected := math.Pi / 2
+	actual := angleToRadians(90)
+	if !utils.IsEqualFloat64(actual, expected) {
+		t.Errorf("Expected value %f is not eqaul with actual value %f", expected, actual)
+	}
+}
+
+func Test_AngleToRadians180(t *testing.T) {
+	expected := math.Pi
+	actual := angleToRadians(180)
+	if !utils.IsEqualFloat64(actual, expected) {
+		t.Errorf("Expected value %f is not eqaul with actual value %f", expected, actual)
+	}
+}
+
+func Test_AngleToRadians360(t *testing.T) {
+	expected := math.Pi * 2
+	actual := angleToRadians(360)
+	if !utils.IsEqualFloat64(actual, expected) {
+		t.Errorf("Expected value %f is not eqaul with actual value %f", expected, actual)
+	}
+}
+
+func Test_AngleToRadiansNeg30(t *testing.T) {
+	expected := -math.Pi / 6
+	actual := angleToRadians(-30)
+	if !utils.IsEqualFloat64(actual, expected) {
+		t.Errorf("Expected value %f is not eqaul with actual value %f", expected, actual)
+	}
+}
+
+func Test_AngleToRadiansNeg60(t *testing.T) {
+	expected := -math.Pi / 3
+	actual := angleToRadians(-60)
+	if !utils.IsEqualFloat64(actual, expected) {
+		t.Errorf("Expected value %f is not eqaul with actual value %f", expected, actual)
+	}
+}
+
+func Test_ComputeFitSize90(t *testing.T) {
+	initial := image.Point{X: 1024, Y: 768}
+	angle := 90.0
+	expected := image.Point{X: 768, Y: 1024}
+	actual := computeFitSize(initial, angleToRadians(angle))
+	if expected != actual {
+		t.Errorf("Expected value %s is not eqaul with actual value %s", expected, actual)
+	}
+}
+
+func Test_ComputeFitSize45(t *testing.T) {
+	initial := image.Point{X: 1024, Y: 768}
+	angle := 45.0
+	expected := image.Point{X: 1267, Y: 1267}
+	actual := computeFitSize(initial, angleToRadians(angle))
+	if expected != actual {
+		t.Errorf("Expected value %s is not eqaul with actual value %s", expected, actual)
+	}
+}
+
+func Test_ComputeFitSize22_5(t *testing.T) {
+	initial := image.Point{X: 1024, Y: 768}
+	angle := 22.5
+	expected := image.Point{X: 1239, Y: 1101}
+	actual := computeFitSize(initial, angleToRadians(angle))
+	if expected != actual {
+		t.Errorf("Expected value %s is not eqaul with actual value %s", expected, actual)
+	}
+}
+
+func Test_ComputeFitSizeNeg22_5(t *testing.T) {
+	initial := image.Point{X: 1024, Y: 768}
+	angle := -22.5
+	expected := image.Point{X: 1239, Y: 1101}
+	actual := computeFitSize(initial, angleToRadians(angle))
+	if expected != actual {
+		t.Errorf("Expected value %s is not eqaul with actual value %s", expected, actual)
+	}
+}
+
+func Test_ComputeFitSizeNeg45(t *testing.T) {
+	initial := image.Point{X: 1024, Y: 768}
+	angle := -45.0
+	expected := image.Point{X: 1267, Y: 1267}
+	actual := computeFitSize(initial, angleToRadians(angle))
+	if expected != actual {
+		t.Errorf("Expected value %s is not eqaul with actual value %s", expected, actual)
+	}
+}
+
+func Test_ComputeOffset90(t *testing.T) {
+	size := image.Point{X: 1024, Y: 768}
+	radians := angleToRadians(90)
+	expected := image.Point{X: -128, Y: 128}
+	actual := computeOffset(size, computeFitSize(size, radians))
+	if expected != actual {
+		t.Errorf("Expected value %s is not eqaul with actual value %s", expected, actual)
+	}
+}
+
+func Test_ComputeOffset180(t *testing.T) {
+	size := image.Point{X: 1024, Y: 768}
+	radians := angleToRadians(180)
+	expected := image.Point{X: 0, Y: 0}
+	actual := computeOffset(size, computeFitSize(size, radians))
+	if expected != actual {
+		t.Errorf("Expected value %s is not eqaul with actual value %s", expected, actual)
+	}
+}
+
+func Test_GetOriginalPixelPosition(t *testing.T) {
+	x := 0
+	y := 0
+	size := image.Point{X: 1024, Y: 768}
+	radians := angleToRadians(90)
+	anchor := image.Point{X: 512, Y: 384}
+	offset := computeOffset(size, computeFitSize(size, radians))
+	expectedX := 1024
+	expectedY := 0
+	actualX, actualY := getOriginalPixelPosition(x, y, radians, anchor, offset)
+	if actualX != expectedX && actualY != expectedY {
+		t.Errorf("Expected value [%d %d] is not eqaul with actual value [%d %d]", expectedX, expectedY, actualX, actualY)
+	}
+}
 
 // -----------------------------Acceptance tests------------------------------------
 func setupTestCaseGray(t *testing.T) *image.Gray {
-	path := "../res/girl.jpg"
+	path := "../res/building.jpg"
 	img, err := imgio.ImreadGray(path)
 	if err != nil {
 		t.Errorf("Could not read image from path: %s", path)
@@ -17,7 +177,7 @@ func setupTestCaseGray(t *testing.T) *image.Gray {
 }
 
 func setupTestCaseRGBA(t *testing.T) *image.RGBA {
-	path := "../res/girl.jpg"
+	path := "../res/building.jpg"
 	img, err := imgio.ImreadRGBA(path)
 	if err != nil {
 		t.Errorf("Could not read image from path: %s", path)
@@ -34,7 +194,7 @@ func tearDownTestCase(t *testing.T, img image.Image, path string) {
 
 func Test_Acceptance_RotateGray90(t *testing.T) {
 	gray := setupTestCaseGray(t)
-	actual, err:= RotateGray(gray, 90, image.Point{X: 201, Y: 201}, true)
+	actual, err := RotateGray(gray, 90, image.Point{X: 512, Y: 384}, true)
 	if err != nil {
 		t.Errorf("Should not throw error!")
 	}
@@ -43,7 +203,7 @@ func Test_Acceptance_RotateGray90(t *testing.T) {
 
 func Test_Acceptance_RotateGray45(t *testing.T) {
 	gray := setupTestCaseGray(t)
-	actual, err:= RotateGray(gray, 45, image.Point{X: 201, Y: 201}, true)
+	actual, err := RotateGray(gray, 45, image.Point{X: 512, Y: 384}, true)
 	if err != nil {
 		t.Errorf("Should not throw error!")
 	}
@@ -52,7 +212,7 @@ func Test_Acceptance_RotateGray45(t *testing.T) {
 
 func Test_Acceptance_RotateGray22(t *testing.T) {
 	gray := setupTestCaseGray(t)
-	actual, err:= RotateGray(gray, 22, image.Point{X: 201, Y: 201}, true)
+	actual, err := RotateGray(gray, 22, image.Point{X: 512, Y: 384}, true)
 	if err != nil {
 		t.Errorf("Should not throw error!")
 	}
@@ -61,7 +221,7 @@ func Test_Acceptance_RotateGray22(t *testing.T) {
 
 func Test_Acceptance_RotateRGBA90(t *testing.T) {
 	rgba := setupTestCaseRGBA(t)
-	actual, err:= RotateRGBA(rgba, 90, image.Point{X: 201, Y: 201}, true)
+	actual, err := RotateRGBA(rgba, 90, image.Point{X: 512, Y: 384}, true)
 	if err != nil {
 		t.Errorf("Should not throw error!")
 	}
@@ -70,7 +230,7 @@ func Test_Acceptance_RotateRGBA90(t *testing.T) {
 
 func Test_Acceptance_RotateRGBA45(t *testing.T) {
 	rgba := setupTestCaseRGBA(t)
-	actual, err:= RotateRGBA(rgba, 45, image.Point{X: 201, Y: 201}, true)
+	actual, err := RotateRGBA(rgba, 45, image.Point{X: 512, Y: 384}, true)
 	if err != nil {
 		t.Errorf("Should not throw error!")
 	}
@@ -79,10 +239,18 @@ func Test_Acceptance_RotateRGBA45(t *testing.T) {
 
 func Test_Acceptance_RotateRGBA22(t *testing.T) {
 	rgba := setupTestCaseRGBA(t)
-	actual, err:= RotateRGBA(rgba, 22, image.Point{X: 201, Y: 201}, true)
+	actual, err := RotateRGBA(rgba, 22, image.Point{X: 512, Y: 384}, true)
 	if err != nil {
 		t.Errorf("Should not throw error!")
 	}
 	tearDownTestCase(t, actual, "../res/transform/roateRGBA22.jpg")
 }
 
+func Test_Acceptance_RotateRGBA45Noresize(t *testing.T) {
+	rgba := setupTestCaseRGBA(t)
+	actual, err := RotateRGBA(rgba, 45, image.Point{X: 512, Y: 384}, false)
+	if err != nil {
+		t.Errorf("Should not throw error!")
+	}
+	tearDownTestCase(t, actual, "../res/transform/roateRGBA45Noresize.jpg")
+}
